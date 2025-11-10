d39daa7ae5ed8122d605b2d49d301784c9128747
GHVBUGB <@example.com>
2025-11-10 16:19:19 +0800

    fix(deploy): change vite output dir to 'dist' for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
@@ -65,7 +65,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
